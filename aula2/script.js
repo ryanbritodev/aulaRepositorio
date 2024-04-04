7a218291d83bcf0a5d2f1c4ecc354c4947f22b3b
Ryan Brito <ryanbritoramos@gmail.com>
2024-04-03 21:36:10 -0300

    Create script.js

---
+++ aula2/script.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual o seu nome?");
+var profissao = prompt("Qual a sua profissão?");
+var idade = prompt("Quantos anos você tem?");
+
+document.write(`Olá ${nome}, você tem ${idade} anos e trabalha como ${profissao}.`);
